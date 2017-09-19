5e9b8e25f38a76591191600534f936bc8e09f513
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-09-19 14:36:16 -0700

    make runner scripts more explicit

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,4 +1,22 @@
-set TEST_PROJECT=.\src\IxMilia.Iges.Test\IxMilia.Iges.Test.csproj
-dotnet restore %TEST_PROJECT%
-if errorlevel 1 exit /b 1
-dotnet test %TEST_PROJECT%
+@echo off
+
+:: build library
+set LIBRARY_PROJECT=%~dp0src\IxMilia.Iges\IxMilia.Iges.csproj
+dotnet restore "%LIBRARY_PROJECT%"
+if errorlevel 1 goto error
+dotnet build "%LIBRARY_PROJECT%"
+if errorlevel 1 goto error
+
+:: build and run tests
+set TEST_PROJECT=%~dp0src\IxMilia.Iges.Test\IxMilia.Iges.Test.csproj
+dotnet restore "%TEST_PROJECT%"
+if errorlevel 1 goto error
+dotnet build "%TEST_PROJECT%"
+if errorlevel 1 goto error
+dotnet test "%TEST_PROJECT%"
+if errorlevel 1 goto error
+goto :eof
+
+:error
+echo Error building project.
+exit /b 1
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -e
 
-TEST_PROJECT=./src/IxMilia.Iges.Test/IxMilia.Iges.Test.csproj
-dotnet restore $TEST_PROJECT
-dotnet test $TEST_PROJECT
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
+
+# build library
+LIBRARY_PROJECT=$_SCRIPT_DIR/src/IxMilia.Iges/IxMilia.Iges.csproj
+dotnet restore "$LIBRARY_PROJECT"
+dotnet build "$LIBRARY_PROJECT"
+
+# build and run tests
+TEST_PROJECT=$_SCRIPT_DIR/src/IxMilia.Iges.Test/IxMilia.Iges.Test.csproj
+dotnet restore "$TEST_PROJECT"
+dotnet build "$TEST_PROJECT"
+dotnet test "$TEST_PROJECT"
